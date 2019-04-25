c169f099e0bdbe847cef2c7c2316e3aa97205ddf
Raphaël Dalmon <raphael.dalmon@epitech.eu>
2019-04-25 22:01:59 +0200

    :fire: Delete inverse function

--- src/divise.c
+++ src/divise.c
@@ -20,17 +20,3 @@ matrix_t *matrix_div(matrix_t *a, matrix_t *b)
         }
     return (matrix);
 }
-
-matrix_t *matrix_inverse(matrix_t *a)
-{
-    matrix_t *matrix;
-
-    if (!a)
-        return (error_ptr(ERROR_NULL_PARAMETER));
-    if (!(matrix = matrix_copy(a)))
-        return (NULL);
-    for (size_t i = 0; i < matrix->rows; i++)
-        for (size_t j = 0; j < matrix->cols; j++)
-            matrix->matrix[i][j] = 1 / matrix->matrix[i][j];
-    return (matrix);
-}
